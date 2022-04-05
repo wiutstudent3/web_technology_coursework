7f1edcd3667ae27e8c36b513ddd7f68b122a6fd3
wiutstudent3 <wiutstudent3@gmail.com>
2022-04-05 15:00:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# STUDENT MANAGEMENT APPLICATION
+
+This is a student management database
+
+#### Instructions
+
+1. Downloading the source code
+
+```bash
+git clone https://github.com/wiutstudentbis/Web_Tech_CW.git
+```
+
+you can download  \*.zip file and open
+
+2. Install dependencies
+
+```bash
+npm install
+```
+
+3. Run the app
+
+```bash
+node index
+```
+
+### Dependencies List
+
+- express
+- sqlite3
+- pug
+
+### Link to github repo
+https://github.com/wiutstudentbis/Web_Tech_CW.git
+
+### Link to the Website URL
+https://handsome-whispering-sign.glitch.me
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
+const sqllite3Conn = require('sqlite3').verbose();
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('assets'));
+app.set('view engine', 'pug');
+
+/// connecting to sqlite3 database
+const dbPath = './data.db';
+
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+  if (err) throw err;
+
+  console.log('Connection successful!');
+});
+
+const select = 'select * from students';
+// Creating a students table
+// db.run(
+//   "create table students(id integer primary key, first_name varchar(100), last_name varchar(100), student_id varchar(100), phone_number varchar(100), email varchar(100))"
+// );
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// show home page
+app.get('/home', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(3000);
